c40e07cc0451c6ce0b67d623337f6e5aa463c4f1
khaivinh3005 <87682358+khaivinh3005@users.noreply.github.com>
2023-07-18 20:31:34 +0700

    Update lesson 1 of JS

---
+++ main.js
@@ -0,0 +1,24 @@
+// khai báo biến  tên biến   gán  giá trị của biến
+// var         box       =       "Kẹo"
+// Kiểu dữ liệu :
+/**
+ * Chuỗi : string
+ * Số : number
+ *    : boolean
+ *    : underfined (khai báo biến nhưng ko gán giá trị)
+ */
+
+/**
+ * Java
+ * Javascript (JS => ES5)
+ */
+
+// Ví dụ 1:
+var numberOne = 10;
+var numberTwo = 2;
+var sum = numberOne + numberTwo;
+var multiple = numberOne * numberTwo;
+var devide = numberOne / numberTwo;
+var miss = numberOne % numberTwo;
+var total = sum + multiple + devide + miss - '2';
+console.log('total: ', total);
