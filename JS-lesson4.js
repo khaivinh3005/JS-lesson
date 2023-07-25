89b506fc7bba001a64aa48c26b61955af4409b45
khaivinh3005 <87682358+khaivinh3005@users.noreply.github.com>
2023-07-25 20:23:55 +0700

    lesson 4 : alert and promt

---
+++ JS-lesson4.js
@@ -0,0 +1,12 @@
+// Hiển thị form nhập nội dung:
+
+var userName = prompt('Vui long nhap ten : ');
+var age = prompt('Vui long tuoi : ');
+
+console.log('userName :', userName);
+console.log('age: ', age);
+
+var showInfo =
+  'Xin chào các bạn, tôi tên là : ' + userName + 'và năm nay tôi : ' + age;
+
+alert(showInfo);
