04f568059a0f934e4789e7d024ce90f11873d640
khaivinh3005 <87682358+khaivinh3005@users.noreply.github.com>
2023-08-08 21:12:49 +0700

    lesson JS 7

--- JS-lesson6(function).js
+++ JS-lesson6(function).js
@@ -3,6 +3,9 @@ var buttonTatDen = document.querySelector('#tatDen');
 var buttonBatDen = document.querySelector('#batDen');
 var theBody = document.querySelector('body');
 var theH1 = document.querySelector('h1');
+var email = document.querySelector('#email');
+var password = document.querySelector('#password');
+var btn = document.querySelector('#btn');
 
 buttonTatDen.addEventListener('click', batVaTatDen);
 buttonBatDen.addEventListener('click', batVaTatDen);
@@ -10,4 +13,18 @@ buttonBatDen.addEventListener('click', batVaTatDen);
 function batVaTatDen() {
   //thuộc tính toggle : nếu thẻ đó có class color-red thì sẽ xoá và ngược lại nếu ko có thì sẽ thêm
   theH1.classList.toggle('color-red');
+}
+
+// TH1
+// btn.addEventListener('click', function submit(event) {
+//   console.log('event : ', event);
+// });
+
+// TH2
+btn.addEventListener('click', showData);
+function showData(event) {
+  // event là thuộc tính có sẵn trong sự kiện click của function
+  // Sự kiện ko cho reload page
+  event.preventDefault();
+  console.log('event: ', event);
 }
---
+++ JS-lesson7.js
@@ -0,0 +1,37 @@
+var numberA = document.getElementById('a').value;
+var numberB = document.getElementById('b').value;
+var numberC = document.getElementById('c').value;
+
+function tinhTong(thamSo1, thamSo2, thamSo3) {
+  var so1 = Number(thamSo1);
+  var so2 = Number(thamSo2);
+  var so3 = Number(thamSo3);
+  /**
+   * th1: có tham số 1 và tham sô 2 và tham số 3
+   * th2: có tham số 1 và tham số 2
+   * Th3: có tham số 1
+   * th4: ko có tham số nào
+   */
+  var tong;
+  if (so1 && so2 && so3) {
+    tong = so1 + so2 + so3;
+  } else if (so1 && so2) {
+    tong = so1 + so2;
+  } else if (so1) {
+    tong = so1;
+  }
+  console.log('tong : ', tong);
+}
+
+tinhTong(numberA, numberB, numberC);
+tinhTong(numberA, numberB);
+tinhTong(numberA);
+
+// Vi du
+// if (numberA > numberB) {
+//   console.log('numberA > number B');
+// } else if (numberA < numberB) {
+//   console.log('numberA < number B');
+// } else {
+//   console.log('numberA = number B');
+// }
