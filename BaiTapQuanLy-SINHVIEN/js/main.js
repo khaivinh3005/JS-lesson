445e1ffd5e653be465f27f7b3a155b7f4331cf3c
khaivinh3005 <87682358+khaivinh3005@users.noreply.github.com>
2023-09-20 21:24:54 +0700

    update ES6

---
+++ BaiTapQuanLy-SINHVIEN/js/main.js
@@ -0,0 +1,32 @@
+// ES5
+// function chucNangA() {
+//   a + b
+// }
+
+// function chucNangA() {
+//     a - b
+// }
+
+// // function ES5
+// function show(name) {
+//   console.log('hi ' + name);
+// }
+
+// show('Long');
+
+// // Function ES6
+// c1 Arrow function
+// const chucNangA = (name, age) => {
+//   let fullName = 'Nguyen';
+//   fullName = fullName + name;
+//   console.log('hi : ', fullName, 'tuoi : ', age);
+// };
+
+// chucNangA('Long ', 18);
+
+// C2;
+// const show = function () {
+//   console.log('hello');
+// };
+
+// BTVN : Trang 37
